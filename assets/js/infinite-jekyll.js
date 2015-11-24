e3020fe90c2644f5db013b55bec359caf7a86996
4711 <bzthcyqullt@mailinator.com>
2015-11-24 14:19:02 +0100

    Update infinite-jekyll.js

--- assets/js/infinite-jekyll.js
+++ assets/js/infinite-jekyll.js
@@ -1,6 +1,3 @@
----
----
-
 $(function() {
 
   var postURLs,
@@ -10,7 +7,7 @@ $(function() {
       loadNewPostsThreshold = 3000;
 
   // Load the JSON file containing all URLs
-  $.getJSON("{{ siteurl }}/all-posts.json", function(data) {
+  $.getJSON("/vw/all-posts.json", function(data) {
     postURLs = data["posts"];
 
     // If there aren't any more posts available to load than already visible, disable fetching
